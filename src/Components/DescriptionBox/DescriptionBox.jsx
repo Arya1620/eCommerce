d2fe37c1df27b22113ab21f63727761e6611f8e3
Arya1620 <arya16200@gmail.com>
2024-02-06 15:08:16 +0530

    made changes

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='desbox'>
+      <div className="desbox-nav">
+        <div className="desbox-nav-box">Description</div>
+        <div className="desbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="desbox-des">
+        <p>Ecommerce is a method of buying and selling goods and services online. The definition of ecommerce business can also include tactics like affiliate marketing. You can use ecommerce channels such as your own website, an established selling website like Amazon, or social media to drive online sales.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default DescriptionBox 
